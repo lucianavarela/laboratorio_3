7a21e0c28d1a01706993b56581756ca197df93d6
alumno <alumno>
2018-05-21 20:57:09 -0300

    sql

---
+++ Programacion/Clase8/Lu/heladeria.sql
@@ -0,0 +1,228 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-05-2018 a las 01:56:22
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `heladeria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(256) NOT NULL,
+  `nacionalidad` varchar(256) NOT NULL,
+  `sexo` varchar(8) NOT NULL,
+  `edad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `nombre`, `nacionalidad`, `sexo`, `edad`) VALUES
+(3, 'Adriel', 'Argentina', 'f', 24),
+(6, 'Adriel', 'Argentina', 'm', 25),
+(4, 'Luciana', 'Argentina', 'f', 24);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `nombre` varchar(128) COLLATE utf8_spanish_ci NOT NULL,
+  `turno` varchar(128) COLLATE utf8_spanish_ci NOT NULL,
+  `tipo` varchar(128) COLLATE utf8_spanish_ci NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `empleado`
+--
+
+INSERT INTO `empleado` (`nombre`, `turno`, `tipo`, `id`) VALUES
+('Agustina', 'mañana', 'jefe', 1),
+('Monica', 'mañana', 'encargado', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `helados`
+--
+
+CREATE TABLE `helados` (
+  `id` int(11) NOT NULL,
+  `sabor` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `precio` float NOT NULL,
+  `tipo` varchar(15) COLLATE utf8_spanish2_ci NOT NULL,
+  `cantidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `helados`
+--
+
+INSERT INTO `helados` (`id`, `sabor`, `precio`, `tipo`, `cantidad`) VALUES
+(1, 'vainilla', 10, 'crema', 100),
+(3, 'vainilla', 10, 'agua', 100),
+(5, 'chocolate', 10, 'agua', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `local`
+--
+
+CREATE TABLE `local` (
+  `id` int(11) NOT NULL,
+  `dirección` varchar(50) NOT NULL,
+  `idLocalidad` int(11) NOT NULL,
+  `estado` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `local`
+--
+
+INSERT INTO `local` (`id`, `dirección`, `idLocalidad`, `estado`) VALUES
+(1, 'Raquel Español 160', 2, 'abierto'),
+(2, 'Raquel Español 160', 10, 'abierto');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `localidad`
+--
+
+CREATE TABLE `localidad` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `provincia` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
+  `estado` varchar(15) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `localidad`
+--
+
+INSERT INTO `localidad` (`id`, `nombre`, `provincia`, `estado`) VALUES
+(1, 'Quilmes', 'Buenos Aires', 'cerrado'),
+(2, 'Avellaneda', 'Buenos Aires', 'abierto'),
+(3, 'Remedios de Escalada', 'Buenos Aires', 'abierto');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `id` int(11) NOT NULL,
+  `idHelado` int(11) NOT NULL,
+  `idLocal` int(11) NOT NULL,
+  `idCliente` int(11) NOT NULL,
+  `idEmpleado` int(11) NOT NULL,
+  `fecha` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `importe` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`,`nacionalidad`,`sexo`,`edad`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `helados`
+--
+ALTER TABLE `helados`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `sabor` (`sabor`,`tipo`);
+
+--
+-- Indices de la tabla `local`
+--
+ALTER TABLE `local`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `localidad`
+--
+ALTER TABLE `localidad`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `helados`
+--
+ALTER TABLE `helados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT de la tabla `local`
+--
+ALTER TABLE `local`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `localidad`
+--
+ALTER TABLE `localidad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
